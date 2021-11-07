54017d6e4100b74c13b79b2c62012f9e9bab5811
odbalapure <odbalapure@gmail.com>
2021-11-07 11:03:06 +0530

    Handling edge cases

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import SingleUser from "./pages/SingleUser";
+import Home from "./pages/Home";
+import CreateUser from "./pages/CreateUser";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/create">
+          <CreateUser />
+        </Route>
+        <Route path="/users/:id">
+          <SingleUser />
+        </Route>
+        <Route path="/*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
